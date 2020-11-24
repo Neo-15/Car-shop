734d0832b12ef9548ac96835fa73c31ce9effe66
Neo-15 <71397872+Neo-15@users.noreply.github.com>
2020-11-24 03:13:58 +0200

    New

--- jquery/apps.js
+++ jquery/apps.js
@@ -6,5 +6,71 @@ $(function() {
 $('.example-car').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 3,
+  dots:true,
 });
+
+$('#hidden-max').click(function() {
+    $('.item-package').toggleClass('active');
+	$('#max').toggleClass('active');
+	
+ });
+
+$('#hidden-dual').click(function() {
+	$('.package-text-dual').toggleClass('active');
+	$('#duals').toggleClass('active');
+ });
+
+$('#individual').click(function() {
+	$('.package-content').toggleClass('active');
+	$('#individualy').toggleClass('active');
+ });
+
+$('#moto').click(function() {
+	$('#moto').toggleClass('active');
+	$('#motos').toggleClass('active');
+ });
+
+$('#sedan').click(function() {
+	$('#sedan').toggleClass('active');
+	$('#sed').toggleClass('active');
+ });
+
+$('#cupe').click(function() {
+	$('#cupe').toggleClass('active');
+	$('#cup').toggleClass('active');
+ });
+
+$('#tesla').click(function() {
+	$('#tesla').toggleClass('active');
+	$('#tes').toggleClass('active');
+ });
+
+$('#underground').click(function() {
+	$('#underground').toggleClass('active');
+	$('#under').toggleClass('active');
+ });
+
+$('#pikap').click(function() {
+	$('#pikap').toggleClass('active');
+	$('#pik').toggleClass('active');
+ });
+
+$('#miniven').click(function() {
+	$('#miniven').toggleClass('active');
+	$('#min').toggleClass('active');
+ });
+
+$('#bus').click(function() {
+	$('#bus').toggleClass('active');
+	$('#buss').toggleClass('active');
+ });
+
+$('.reviev-content').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots:true,
+  arrows:true,
+});
+
